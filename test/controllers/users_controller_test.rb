6577e44a7dac9d2f91a98083f637894f64f3798b
saeki <saekitoda@gmail.com>
2019-06-04 20:02:39 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-    test "should redirect edit when not logged in" do
+  test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
@@ -30,7 +30,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
-    test "should redirect destroy when not logged in" do
+  test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
@@ -43,6 +43,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+  
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
 
